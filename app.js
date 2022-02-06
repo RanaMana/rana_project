855a4ab14e0469feafad650fb29929eda5150f6d
Majed Al Thagafi <malthagafi@provensolution.com>
2022-02-06 16:09:45 +0300

    console log app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function togglePass() {
   if (document.getElementById("pass").getAttribute("type") == "password") {
     document.getElementById("pass").setAttribute("type", "text");
     document.getElementById("icon").setAttribute("class", "fa fa-eye-slash");
+    console.log("Majed was here!");
   } else {
     document.getElementById("pass").setAttribute("type", "password");
     document.getElementById("icon").setAttribute("class", "fa fa-eye");
